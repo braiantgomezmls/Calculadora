988d126c95b158f6728bdd70cf6985a6af79a6ee
Pc12 <Pc12@Villapc12>
2026-01-31 09:54:39 -0600

    Master

---
+++ src/calculadora2/Calculadora2.java
@@ -0,0 +1,26 @@
+
+package calculadora2;
+
+import java.util.Scanner;
+
+public class Calculadora2 {
+
+  
+    public static void main(String[] args) {
+              Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingresa un numero");
+        int a = sc.nextInt();
+        
+        System.out.println("Ingresa el segundo numero");
+        int b = sc.nextInt();
+        
+        //Division div = new Division();
+        //Multiplicacion multi = new Multiplicacion();
+        
+        //System.out.println("El resultado es es: "+div.calculadora(a,b));
+       // System.out.println("El resultado es: "+multi.calculadora(a,b));
+    }
+    }
+    
+
